492c5ec9d5a9ad14c70978587d73113ad7bd5a8d
Afnan Abbasi <afnanabbasi0k@gmail>
2024-03-23 12:24:21 +0500

    JavaScript-Patterns-Program

---
+++ app.js
@@ -0,0 +1,108 @@
+document.write('<h1>Task 01</h1>')
+for (var i = 1; i <= 7; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write(j + " ")
+    }
+    document.write("<br>")
+}
+document.write('<h1>Task 02</h1>')
+for (var i = 7; i >= 1; i--) {
+    for (var j = 1; j <= i; j++) {
+        document.write(j + " ")
+    }
+    document.write("<br>")
+}
+document.write('<h1>Task 03</h1>')
+for (var i = 1; i <= 7; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write('*' + ' ')
+    }
+    document.write('<br>')
+}
+document.write('<h1>Task 04</h1>')
+for (var i = 7; i >= 1; i--) {
+    for (var j = 1; j <= i; j++) {
+        document.write('*' + ' ')
+    }
+    document.write('<br>')
+}
+document.write('<h1>Task 05</h1>')
+for (var i = 1; i <= 8; i++) {
+    for (var j = 8; j > i; j--) {
+        document.write("  &nbsp;  ");
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write(k + '     ');
+    }
+    document.write('<br>');
+}
+document.write('<h1>Task 06</h1>')
+for (var i = 7; i >= 1; i--) {
+    for (var j = 7; j >= i; j--) {
+        document.write('&nbsp')
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write(k + ' ')
+    }
+    document.write('<br>');
+}
+document.write('<h1>Task 07</h1>')
+for (var i = 1; i <= 8; i++) {
+    for (var j = 8; j > i; j--) {
+        document.write('&nbsp&nbsp')
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write('*' + ' ')
+    }
+    document.write('<br>');
+}
+document.write('<h1>Task 08</h1>')
+for (var i = 8; i >= 1; i--) {
+    for (var j = 8; j > i; j--) {
+        document.write('&nbsp&nbsp')
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write('*' + ' ')
+    }
+    document.write('<br>');
+}
+document.write('<h1>Task 09</h1>')
+for (var i = 1; i <= 8; i++) {
+    for (var j = 8; j > i; j--) {
+        document.write('&nbsp&nbsp&nbsp')
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write(k + ' ')
+    }
+    document.write('<br>');
+}
+document.write('<h1>Task 10</h1>');
+for (var i = 7; i >= 1; i--) {
+    for (var j = 7; j > i; j--) {
+        document.write('&nbsp&nbsp&nbsp;');
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write(k + " ");
+    }
+    document.write("<br>");
+}
+document.write('<h1>Task 11</h1>');
+for (var i = 1; i <= 7; i++) {
+    for (var j = 7; j > i; j--) {
+        document.write("&nbsp&nbsp&nbsp")
+    } for (var k = 1; k <= i; k++) {
+        document.write("*" + ' ')
+    }
+    document.write('<br>')
+}
+document.write('<h1>Task 12</h1>');
+for (var i = 7; i >= 1; i--) {
+    for (var j = 7; j > i; j--) {
+        document.write("&nbsp&nbsp&nbsp")
+    }
+    for (var k = 1; k <= i; k++) {
+        document.write("*" + " ")
+    }
+    document.write('<br>')
+}
+
